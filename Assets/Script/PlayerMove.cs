fbe9cbac65fd3e65546e0e960ba6817de4d55ce2
DESKTOP-ATAQR4T\Dove <99jinjw@naver.com>
2025-10-08 23:26:27 +0900

    사운드(관련 이벤트) Ray로 구현 중

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.UI.Image;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -10,7 +11,12 @@ public class PlayerMove : MonoBehaviour
     bool moveKeyDown = false;
     float movetime = 0;
 
-    public LayerMask enemyLayer;  
+    public LayerMask enemyLayer;
+    public LayerMask wallLayer;
+
+    public int rayCount = 30;
+    public int collisionRayCount = 2;
+
     public float walkNoiseRadius = 3f;
 
     void Start()
@@ -36,7 +42,7 @@ public class PlayerMove : MonoBehaviour
         if (movetime >= 0.5f)
         {
             MakeNoise(walkNoiseRadius);
-
+            noiseRay(transform.position, walkNoiseRadius, collisionRayCount, rayCount, 1.0f);
             movetime = 0;
         }
     }
@@ -53,6 +59,39 @@ public class PlayerMove : MonoBehaviour
                 enemy.GetPoint(transform.position);
             }
         }
+    }
+
+    void noiseRay(Vector3 pos , float radius, float deptm, int rayCount ,float intensity)
+    {
+
+        if (deptm <= 0 || intensity <= 0.1f)
+            return;
+     
+
+        for (int i = 0; i < rayCount; i++)
+        {
+            Vector3 sRay = Random.onUnitSphere; //구의 표면에 있는 점을 임의로 선택하여 반환합니다. 즉 랜덤 위치로 반환
+            if (sRay.y < 0)
+                continue;
+           
+            if (Physics.Raycast(pos, sRay ,out RaycastHit hit , enemyLayer | wallLayer))
+            {
+                Debug.DrawRay(pos, sRay * hit.distance, Color.yellow, 0.5f);
+                EnemyMove enemy = hit.collider.GetComponent<EnemyMove>();
+                if (enemy != null)
+                {
+                    enemy.GetPoint(pos);
+                }
+
+                if(((hit.collider.gameObject.layer) == wallLayer ) && deptm > 0)
+                {
+                    Vector3 reRay = Vector3.Reflect(pos, hit.normal); //Reflect ->백터를 반사시키는 함수
+                    float reInten = intensity / 2;
+                    noiseRay(pos + sRay * 0.1f, radius * 0.7f, deptm - 1, rayCount / 2, reInten);
+                }
+            }
+        }
+
     }
 
 
